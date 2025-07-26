5b513b27a11df45731c26bf6d23e9535d0422df5
Miftau <miftaudeenb@gmail.com>
2025-07-26 15:46:00 +0100

    cron job added

--- app.py
+++ app.py
@@ -4,8 +4,28 @@ from models import db
 from admin.routes import admin_bp
 from api.routes import api_bp
 from flask_cors import CORS
+from apscheduler.schedulers.background import BackgroundScheduler
+import requests
 
 app = Flask(__name__)
+
+PING_URL = "https://movierecommender-i9ne.onrender.com/"
+def keep_alive():
+    try:
+        response = requests.get(PING_URL)
+        print(f"[Keep Alive] Status: {response.status_code}")
+    except Exception as e:
+        print(f"[Keep Alive] Failed: {e}")
+
+# Schedule the job every 10 minutes
+scheduler = BackgroundScheduler()
+scheduler.add_job(func=keep_alive, trigger="interval", minutes=10)
+scheduler.start()
+
+# Shut down scheduler when exiting the app
+import atexit
+atexit.register(lambda: scheduler.shutdown())
+
 CORS(app)
 app.config.from_object(Config)
 
